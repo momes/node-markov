e4b1dc4a726b64589c3630f52b96c6c39b991221
momes <mo.enokida@gmail.com>
2021-05-11 17:13:27 -0700

    so messy

--- markov.js
+++ markov.js
@@ -1,3 +1,49 @@
+
+"use strict";
+
+const fsP = require('fs/promises');
+const fs = require('fs');
+const PATH = './eggs.txt';
+var text
+
+async function cat(path){
+  let contents;
+    try {
+        contents = await fsP.readFile(path, "utf8");
+        // console.log(contents);
+        
+    } catch (err) {
+        console.log(`Error reading ${path}: 
+            Error: ENOENT: no such file or directory, open '${path}'`)
+        process.exit(1);
+    }
+    text = contents;
+    // console.log('type of text ---->', typeof text);
+    let mm = new MarkovMachine(text);
+    console.log(mm.words);
+}
+
+//let text_test = cat(PATH);
+// console.log('text is --->', text_test)
+
+
+
+
+// const TEXT = fs.readFile('./eggs.txt', "utf8");
+
+// const argv = process.argv;
+// async function cat(){
+//       try {
+//         let contents = await fs.readFile('./eggs.txt', "utf8");
+
+//     } catch (err) {
+//         console.log(`Error reading ${argv[2]}: 
+//             Error: ENOENT: no such file or directory, open '${argv[2]}'`)
+//         process.exit(1);
+//     }
+// }
+// cat();
+
 /** Textual markov chain generator */
 
 
@@ -7,7 +53,9 @@ class MarkovMachine {
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
+   
     // MORE CODE HERE
+    // let chains = this.makeChains();
   }
 
   /** set markov chains:
@@ -17,6 +65,9 @@ class MarkovMachine {
 
   makeChains() {
     // MORE CODE HERE
+    let unique_words = new Set(this.words);
+    console.log(unique_words);
+
   }
 
 
@@ -26,3 +77,9 @@ class MarkovMachine {
     // MORE CODE HERE
   }
 }
+
+text = fs.readFileSync("./eggs.txt", "utf-8")
+//console.log(text);
+let words = text.split(/[ \r\n]+/);
+//let mm = new MarkovMachine(text);
+console.log(words)
